92cdc2f7755cb138f270f727033e6a8488ea8a99
lahlaliwassim2 <wlahlali34@gmail.com>
2023-03-03 22:06:25 +0100

    ajouter l'objet contenent les questions et ajouter la fonction qui permet de calculer le score en bouclant sur les question et en verifiant si la repons selectionner et la bonne question

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,47 @@
 <script setup>
-
+import { ref, computed } from 'vue'
+const questions = ref([
+  {
+	question: 'What is Vue?',
+	answer: 0,
+	options: [
+		'A framework',
+		'A library',
+		'A type of hat'
+	],
+	selected: null
+  },
+  {
+	question: 'What is Vuex used for?',
+	answer: 2,
+	options: [
+		'Eating a delicious snack',
+		'Viewing things',
+		'State management'
+	],
+	selected: null
+  },
+  {
+	question: 'What is Vue Router?',
+	answer: 1,
+	options: [
+		'An ice cream maker',
+		'A routing library for Vue',
+		'Burger sauce'
+	],
+	selected: null
+  }
+])
+const currentQuestion = ref(0)
+const quizCompleted = ref(false)
+const score = computed(()=>{
+  let value = 0
+  questions.value.map(q=>{
+    if(q.selected == q.answer){
+      value++
+    }
+  })
+})
 </script>
 
 <template>

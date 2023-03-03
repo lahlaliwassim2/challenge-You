b0e005678c2b24061350cff2343b67180d16df31
lahlaliwassim2 <wlahlali34@gmail.com>
2023-03-03 16:30:06 +0100

    building the app

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>Hello world</h1>
+</template>
+
+<style scoped>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Montsrrat',sans-serif;
+}
+
+body {
+  background-color: bisque;
+  color: antiquewhite;
+}
+</style>

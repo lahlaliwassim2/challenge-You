b7b0858119b2d0fc8d73521dc13b4e3d3fcb4571
lahlaliwassim2 <wlahlali34@gmail.com>
2023-03-04 17:02:16 +0100

    ajouter la template pour le corp de l aplication

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,83 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed } from "vue";
 const questions = ref([
   {
-	question: 'What is Vue?',
-	answer: 0,
-	options: [
-		'A framework',
-		'A library',
-		'A type of hat'
-	],
-	selected: null
+    question: "What is Vue?",
+    answer: 0,
+    options: ["A framework", "A library", "A type of hat"],
+    selected: null,
   },
   {
-	question: 'What is Vuex used for?',
-	answer: 2,
-	options: [
-		'Eating a delicious snack',
-		'Viewing things',
-		'State management'
-	],
-	selected: null
+    question: "What is Vuex used for?",
+    answer: 2,
+    options: ["Eating a delicious snack", "Viewing things", "State management"],
+    selected: null,
   },
   {
-	question: 'What is Vue Router?',
-	answer: 1,
-	options: [
-		'An ice cream maker',
-		'A routing library for Vue',
-		'Burger sauce'
-	],
-	selected: null
-  }
-])
-const currentQuestion = ref(0)
-const quizCompleted = ref(false)
-const score = computed(()=>{
-  let value = 0
-  questions.value.map(q=>{
-    if(q.selected == q.answer){
-      value++
+    question: "What is Vue Router?",
+    answer: 1,
+    options: [
+      "An ice cream maker",
+      "A routing library for Vue",
+      "Burger sauce",
+    ],
+    selected: null,
+  },
+]);
+const currentQuestion = ref(0);
+const quizCompleted = ref(false);
+const score = computed(() => {
+  let value = 0;
+  questions.value.map((q) => {
+    if (q.selected == q.answer) {
+      value++;
     }
-  })
-  return value
-})
-const getCurrentQuestion = computed(()=>{
-  let question = questions.value[currentQuestion.value]
-  questions.index = currentQuestion.value
-  return question
-})
+  });
+  return value;
+});
+const getCurrentQuestion = computed(() => {
+  let question = questions.value[currentQuestion.value];
+  questions.index = currentQuestion.value;
+  return question;
+});
 
-const setAnswer = evt=>{
-  questions.value[currentQuestion.value].selected = evt.target.value
-  evt.target.value = null
-}
+const setAnswer = (evt) => {
+  questions.value[currentQuestion.value].selected = evt.target.value;
+  evt.target.value = null;
+};
 
-const nextQuestion = ()=>{
-  if(currentQuestion.value < questions.value.lenght - 1){
-    currentQuestion.value++
-  } else{
-    quizCompleted.value = true
+const nextQuestion = () => {
+  if (currentQuestion.value < questions.value.lenght - 1) {
+    currentQuestion.value++;
+  } else {
+    quizCompleted.value = true;
   }
-}
+};
 </script>
 
 <template>
-    <h1>Hello world</h1>
+  <main class="app">
+    <h1>Quiz</h1>
+    <section class="quiz">
+      <div class="quiz-info">
+        <span class="question">{{ getCurrentQuestion.question }}</span>
+        <span class="score">Score : {{ score }} / {{ questions.length }}</span>
+      </div>
+      <div class="options">
+        <label >
+          <input type="radio" :name="getCurrentQuestion.index" :value="index" />
+        </label>
+      </div>
+    </section>
+  </main>
 </template>
 
 <style scoped>
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: 'Montsrrat',sans-serif;
+  font-family: "Montsrrat", sans-serif;
 }
 
 body {
